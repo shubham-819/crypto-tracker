46e4e181a97024e581105cd887510cff3dac9be6
shubham-819 <94047251+shubham-819@users.noreply.github.com>
2022-09-15 20:41:46 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCD1mQm3vcWsIYXXdb_kq8PJMZWUY32Sds",
+  authDomain: "crypto-tracker-d2243.firebaseapp.com",
+  projectId: "crypto-tracker-d2243",
+  storageBucket: "crypto-tracker-d2243.appspot.com",
+  messagingSenderId: "798444446820",
+  appId: "1:798444446820:web:5bbfcab5533eccb5802b71"
+};
+
+export default firebaseConfig;
